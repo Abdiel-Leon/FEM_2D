53297a288e649569e5bc95d2535c22e8fb594a42
Abdiel-Leon <abdielramon_leon@hotmail.com>
2023-07-17 21:09:08 +0200

    Final commit

--- FEM_2D_Triangle_bechmark2.py
+++ FEM_2D_Triangle_bechmark2.py
@@ -12,6 +12,7 @@ Created on Wed Mar  8 17:04:45 2023
 @author: abdiel
 
 ### Code to produce FEM analyses in 2D for very simple geometries###
+Benchmark adopted from Finite Element Method SME 3033 notes by Dr. Nazri Kamsah 
 """
 import numpy
 #from operator import itemgetter
